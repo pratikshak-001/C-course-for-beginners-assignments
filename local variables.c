f1c409dde64797980fb86cbabb3ed161041c446e
pratikshak-001 <pratikshakvibes@gmail.com>
2026-03-25 14:42:19 +0530

    Add local variables and correct printf syntax
    Fix formatting issues in printf statements.

---
+++ local variables.c
@@ -0,0 +1,18 @@
+# include<stdio.h>
+
+void test(){
+  int a=50;
+  int b=67;
+
+printf("\n Values : a=%d and b=%d",a,b);
+}
+
+int main(){
+  int x =89;
+  int y = 67;
+
+printf("\n Values : x =%d and y=%d ,x,y);
+test();
+
+return 0;
+}
